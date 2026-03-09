97ceccacd16bf434114980d5ec30075d1fed680d
Diego <diegoreyesbarraza@hotmail.com>
2026-03-09 01:08:13 -0700

    Estableciendo las restAPI que conectan con la capa de conexion

--- src/routers/productRouters.ts
+++ src/routers/productRouters.ts
@@ -1,25 +1,18 @@
-import { Router, Request, Response } from 'express';
-const productRouter:Router = Router();
+import { Router } from 'express';
+import { createProduct, deleteProduct, getAllProducts, getProductById, modifyProduct} from '../controllers/productController.js'; 
 
-productRouter.get('/', (req:Request, res:Response) => {
-    res.send('Get a list of products')
-});
+const productRouter:Router = Router();  
 
-productRouter.get('/:id', (req:Request, res:Response) => {
-    res.send(`Get the product ${req.params.id}`)
-});
+productRouter.get('/', getAllProducts);  
 
-productRouter.post('/:id', (req:Request, res:Response) => {
-    res.send(`Create a new product with ID: ${req.body.id}`)
-})
+productRouter.get('/:id', getProductById);  
 
-productRouter.patch('/:id', (req:Request, res:Response) => {
-    res.send(`Update the product ${req.params.id} with the values of 
-    ${req.body.name}, ${req.body.price} and ${req.body.stock}`)
-});
+productRouter.post('/', createProduct);  
 
-productRouter.delete('/', (req:Request, res:Response) => {
-    res.send(`Deleting the product ${req.body.id}`)
-});
+productRouter.patch('/:id', modifyProduct);  
 
-export default productRouter;
+productRouter.delete('/', deleteProduct);  
+
+export default productRouter; 
+
+
